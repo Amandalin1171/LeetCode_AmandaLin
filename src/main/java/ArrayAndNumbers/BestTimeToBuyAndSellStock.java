59dddbda1ea2959a6132d4de9ade131274d9a485
linyue2 <you@example.com>
2020-02-24 01:25:58 -0800

    add DP

--- src/main/java/ArrayAndNumbers/BestTimeToBuyAndSellStock.java
+++ src/main/java/ArrayAndNumbers/BestTimeToBuyAndSellStock.java
@@ -1,5 +1,55 @@
 package ArrayAndNumbers;
 
+/**
+ * 题目：
+ * 121. Best Time to Buy and Sell Stock
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ *
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ *
+ * Note that you cannot sell a stock before you buy one.
+ *
+ * Example 1:
+ *
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ *              Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * Example 2:
+ *
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+
+/**
+ * 笔记：
+ * 类似于DP的思想：站在此刻，我有两个选择，把我脚下的这个数字进行操作（加到subarray中)或者不管
+ * 只不过这里面简化就是直接进行判断，如果加进去发现小于0，直接赋值变成0：
+ * Kadane's Algorithm： the logic is to calculate the difference (maxCur += prices[i] - prices[i-1])
+ * of the original array, and find a contiguous subarray giving maximum profit.
+ * If the difference falls below 0, reset it to zero.
+ *
+ * ******快去看下一个题，打开了DP新世界的大门！！！*******
+ * Maximum Subarray，一样的思想套路！！！但是已经是totally DP的思想咯！！！加油~~~
+ */
 public class BestTimeToBuyAndSellStock {
+  public int maxProfit(int[] prices) {
+    int max = 0;
+    int curMax = 0;
+    for(int i = 0; i < prices.length - 1; i++) {
+      curMax = Math.max(0, curMax + prices[i + 1] - prices[i]);
+      max = Math.max(max, curMax);
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    BestTimeToBuyAndSellStock testClass = new BestTimeToBuyAndSellStock();
+    int[] prices = {7,1,5,3,6,4};
+    int res = testClass.maxProfit(prices);
+    System.out.println(res);
+  }
 
 }
--- src/main/java/ArrayAndNumbers/MaximumSubarray.java
+++ src/main/java/ArrayAndNumbers/MaximumSubarray.java
@@ -1,5 +1,47 @@
 package ArrayAndNumbers;
 
+/**
+ * 题目：
+ * 53. Maximum Subarray
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ *
+ * Example:
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Follow up:
+ *
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+
+/**
+ * 笔记：
+ * 需要注意的点都在行间附注，就是考虑处在一个时点的时候面临的选择^^
+ */
+
 public class MaximumSubarray {
+  public int maxSubArray(int[] nums) {
+    int curSum = nums[0];
+    int maxSum = nums[0];
+    //注意一开始取了第一个值，所以for loop的时候i从1开始取值，千万别大脑没动手先行--#
+    for(int i = 1; i < nums.length; i++) {
+      //站在第二个数开始，往后每一个数字，我们都面临两个选择：
+      //选择1： 把这个数算进之前的合计数中： curSum + nums[i]
+      //选择2： 前面的都不要，这个数开始从新算： nums[i]
+      //所以每一步的curSum就是取这两个选择的最大值
+      curSum = Math.max(curSum + nums[i], nums[i]);
+      maxSum = Math.max(maxSum, curSum);
+    }
+    return maxSum;
+  }
+
+  public static void main(String[] args) {
+    MaximumSubarray testClass = new MaximumSubarray();
+    int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+    int res = testClass.maxSubArray(nums);
+    System.out.println(res);
+  }
 
 }
